e8c37bf4d16b139b864850761bbe620c4c8409c5
Ashiori0932 <144766213+Ashiori0932@users.noreply.github.com>
2026-08-15 11:45:41 +0800

    fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/plugin-timeline-studio-vite/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
